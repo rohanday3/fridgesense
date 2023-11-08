2f58d31dba4ea44006b52745bc98cf2d8e2fc716
rohanday3 <rohanday4@gmail.com>
2023-11-08 18:13:49 +0200

    Updated

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,12 +4,16 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
 import pandas as pd
+import datetime
+import plotly.express as px
+import plotly.graph_objects as go
 
 # Initialize firebase app with credentials
-cred = credentials.Certificate("fridgemon-355420-b1b55a0a251b.json")
-firebase_admin.initialize_app(cred, {
-    'databaseURL': 'https://fridgemon-355420-default-rtdb.firebaseio.com/'
-})
+if not firebase_admin._apps:
+    cred = credentials.Certificate("fridgemon-355420-b1b55a0a251b.json")
+    firebase_admin.initialize_app(cred, {
+        'databaseURL': 'https://fridgemon-355420-default-rtdb.firebaseio.com/'
+    })
 
 # Get reference to the temperature data in firebase
 ref = db.reference('sensor')
@@ -26,19 +30,67 @@ ref = db.reference('sensor')
 # display the latest temperature, humidity, and pressure and the time it was recorded
 latest = ref.order_by_key().limit_to_last(1).get()
 latest = list(latest.values())[0]
-st.write("Latest temperature: ", latest['temperature'])
-st.write("Latest humidity: ", latest['humidity'])
-st.write("Latest pressure: ", latest['pressure'])
-st.write("Latest time: ", latest['date'])
+# st.write("Latest temperature: ", latest['temperature'])
+# st.write("Latest humidity: ", latest['humidity'])
+# st.write("Latest pressure: ", latest['pressure'])
+# st.write("Latest time: ", latest['date'])
+
+
+st.title('Fridge Monitor')
+st.header('Current Temperature')
+# st.subheader('Current temperature is ' + str(latest['temperature']) + ' C')
+
+fig_current = go.Figure(go.Indicator(
+    mode = "gauge+number",
+    value = latest['temperature'],
+    domain = {'x': [0, 1], 'y': [0, 1]},
+    gauge = {'axis': {'range': [0, 10]},
+             'bar': {'thickness': 0},
+             'steps' : [
+                 {'range': [0, 2], 'color': "orange"},
+                 {'range': [2, 8], 'color': "lightblue"},
+                 {'range': [8, 10], 'color': "orange"}],
+             'threshold' : {'line': {'color': "black", 'width': 5}, 'thickness': 0.75, 'value': latest['temperature']}}))
+
+fig_current.update_layout(title='Current Temperature', font_size=16)
+
+st.plotly_chart(fig_current)
 
 # get the last 30 days of temperature data using the date "date": "2023-11-08 14:27:01"
-last1000 = ref.order_by_key().limit_to_last(1000).get()
+all_records = ref.order_by_key().get()
 # make a dataframe from the data with date and temperature columns
-df = pd.DataFrame.from_records(list(last1000))
+df = pd.DataFrame.from_records(all_records)
+df = df.transpose()
 df['date'] = pd.to_datetime(df['date'])
-# limit the dataframe to the last 30 days
-df = df[df['date'] > pd.Timestamp.now() - pd.Timedelta(days=30)]
 df = df.set_index('date')
-st.line_chart(df['temperature'], key='temp', use_container_width=True, height=300, width=300, title='Temperature', x_axis='Date', y_axis='Temperature (C)', help='Temperature over the last month')
+# limit the dataframe to the last 30 days
+# Get the selected timeframe
+timeframe = st.date_input('Select timeframe', [datetime.date.today() - datetime.timedelta(days=7), datetime.date.today()])
 
-# display a line chart of the average temperature per day over the last month
+# Check if two dates are selected
+if len(timeframe) == 2:
+    start_date, end_date = timeframe
+else:
+    # If not, set default values
+    start_date = datetime.date.today() - datetime.timedelta(days=7)
+    end_date = datetime.date.today()
+df = df[(df.index >= pd.Timestamp(start_date)) & (df.index <= pd.Timestamp(end_date))]
+
+# Resample the DataFrame to a daily frequency and calculate the mean temperature for each day
+df_daily = df.resample('D').mean()
+
+fig = px.line(df, x=df.index, y='temperature', labels={'x':'Date', 'y':'Temperature (C)', 'title':'Temperature History'})
+
+fig.update_layout(title='Temperature History', xaxis_title='Date', yaxis_title='Temperature (C)')
+
+st.header('Temperature History')
+
+st.plotly_chart(fig)
+
+fig_curved = go.Figure()
+
+fig_curved.add_trace(go.Scatter(x=df_daily.index, y=df_daily['temperature'], mode='lines', line_shape='spline', name='temperature'))
+
+fig_curved.update_layout(title='Average Daily Temperature History', xaxis_title='Date', yaxis_title='Temperature (C)')
+
+st.plotly_chart(fig_curved)
