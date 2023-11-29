72a7c0015454c67d62ef42e029058ee166d4fb08
rohanday3 <rohanday4@gmail.com>
2023-11-29 11:08:47 +0200

    Moved secrets to .env

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,9 +10,22 @@ import plotly.graph_objects as go
 
 # Initialize firebase app with credentials
 if not firebase_admin._apps:
-    cred = credentials.Certificate("fridgemon-355420-b1b55a0a251b.json")
+    # use streamlit secrets to get the firebase credentials
+    cred = credentials.Certificate({
+        "type": "service_account",
+        "project_id": st.secrets["project_id"],
+        "private_key_id": st.secrets["private_key_id"],
+        "private_key": st.secrets["private_key"],
+        "client_email": st.secrets["client_email"],
+        "client_id": st.secrets["client_id"],
+        "auth_uri": st.secrets["auth_uri"],
+        "token_uri": st.secrets["token_uri"],
+        "auth_provider_x509_cert_url": st.secrets["auth_provider_x509_cert_url"],
+        "client_x509_cert_url": st.secrets["client_x509_cert_url"],
+        "universe_domain": st.secrets["universe_domain"],
+    })
     firebase_admin.initialize_app(cred, {
-        'databaseURL': 'https://fridgemon-355420-default-rtdb.firebaseio.com/'
+        'databaseURL': st.secrets["databaseURL"]
     })
 
 # Get reference to the temperature data in firebase
